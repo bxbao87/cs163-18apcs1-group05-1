d163021ea4d4d70c8cecc2080c638f7d8e6fc228
Nhan Lun <bebi10181@yahoo.com.vn>
2019-07-13 16:36:19 +0700

    created class search

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
 #include "Search.h"
-#include "frontEnd.h"
 
 int main()
 {
-	/*Search bogoSearch;
-	bogoSearch.run();*/
-	Trie trie;
-	std::string tmp = "ahihi";
-	std::string inFile = "nothing";
-	/*trie.AddKey(tmp, inFile);
-	tmp = "yolo";
-	inFile = "something";
-	trie.AddKey(tmp, inFile);*/
-	//trie.SaveTrie("Process\\Index.txt");
-	//FrontEnd();
-	/*trie.LoadTrie("Process\\Index.txt");
-	auto a = trie.GetKey(tmp);
-	for (auto i : a)
-		std::cout << i << ' ';*/
-
+	Search bogoSearch;
+	bogoSearch.Run();
 	system("pause");
 	return 0;
 }
