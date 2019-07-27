e51d01a309694513eecdbd8862c00a103adef4db
khaluan <pkluan18@apcs.vn>
2019-07-27 10:55:28 +0700

    Fake commit

--- main.cpp
+++ main.cpp
@@ -6,36 +6,35 @@
 int main()
 {
 	Search bogoSearch;
-	bogoSearch.Run();
+	//bogoSearch.Run();
 	//std::string query("la la land AND (\"made in China\" OR intitle:something good");
 	//std::cout << bogoSearch.InfixToPostfix(query);
 	//FrontEnd();
 	//bogoSearch.InputKey(28, 21);
 	//auto a = bogoSearch.GetFilename("Data");
 	//system("Start Data/1.txt");
-
-	// while (true) {
-	// 	std::cout << "Search normal" << std::endl;
-	// 	std::string query;
-	// 	getline(std::cin, query);
-	// 	std::vector<std::string> keyword = splitSentence(query);
-	// 	std::string tmp = query;
-	// 	query = bogoSearch.InfixToPostfix(query);
-	// 	std::cout << query << '\n';
-	// 	auto v = bogoSearch.Process(query);
-	// 	std::cout << v.size() << std::endl;
-	// 	bogoSearch.Debug(v);
-	// }
+	ExitScreen();
+	system("pause");
+	while (true) {
+		system("cls");
+		std::cout << "Search normal" << std::endl;
+		std::string query;
+		getline(std::cin, query);
+		std::vector<std::string> keyword = splitSentence(query);
+		std::string tmp = query;
+		query = bogoSearch.InfixToPostfix(query);
+		auto v = bogoSearch.Process(query);
+		
 		std::vector<std::string> fileList;
 		bogoSearch.GetFileNameByInt(v, fileList);
 		std::vector<Document> result;
 		CreateVectorDoc(fileList, result);
 		/*std::cout << v.size() << std::endl;
-		bogoSearch.Debug(v);*/
+	//	bogoSearch.Debug(v);*/
 		int t = 0;
 		std::cout << result.size() << std::endl;
 		Sleep(1000);
-		for (auto i:result)
+		for (auto i : result)
 		{
 			i.GetParagraphForShowing(keyword, tmp);
 			i.DisplayResult(5, 5 + t++ * 10);
@@ -43,9 +42,9 @@ int main()
 			Sleep(2000);
 		}
 		_getch();
-		/*for(auto i:v)
-		system("pause");*/
-	}
+	}/*for(auto i:v)
+	//	system("pause");*/
+	//}
 	/*std::string query = "de nhi quoc su";
 	auto v=bogoSearch.SearchExact(query);
 	std::vector<std::string> keyword = splitSentence(query);
