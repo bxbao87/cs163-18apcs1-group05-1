2b7d4c06ca2baaff7a20632d8f1d9385943f363b
khaluan <pkluan18@apcs.vn>
2019-07-28 21:12:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,21 @@
 int main() {
 	Search bogoSearch;
 	bogoSearch.Run();
-	/// Luan's draft
+	/*///Luan's draft starts here
 	while (true) {
 		std::string query;
 		std::getline(std::cin, query);
-		bogoSearch.SearchPlaceHolder(query);
-		std::cout << query << std::endl;
+		std::vector <std::string> title, content;
+		bogoSearch.SplitQuery(query, title, content);
+		std::cout << "TITLE" << std::endl;
+		for (auto word : title)
+			std::cout << word << std::endl;
+		std::cout << std::endl;
+		std::cout << "CONTENT" << std::endl;
+		for (auto word : content) {
+			std::cout << word << std::endl;
+		}
 	}
-	///End of Luan's draft
-	system("pause");
+	///End of Luan's draft*/
 	return 0;
 }
